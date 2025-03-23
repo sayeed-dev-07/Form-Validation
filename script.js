81adec73f2c205fedd4930be5c144fd2bd4c4ece
Sayeed <expsayeedshorif@gmail.com>
2025-03-23 10:01:14 +0600

    added all html and some css file

---
+++ script.js
@@ -0,0 +1,7 @@
+const form = document.getElementById("form");
+const email = document.getElementById("email");
+const country = document.getElementById("country");
+const code = document.getElementById("code");
+const password = document.getElementById("password");
+const confirmPassword = document.getElementById("confirm-password");
+const submitBtn = document.querySelector("button");
